d54e759a97e1c4823d128b11eee0d5b0d75abc85
Martín Caro <martincaro98@gmail.com>
2024-11-17 18:20:47 -0300

    [A] Blockout Level

--- Scenes/Main/player.gd
+++ Scenes/Main/player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -700.0
 
 
 func _physics_process(delta):
 	# Add the gravity.
+	var gravity = get_gravity() *1.6
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
