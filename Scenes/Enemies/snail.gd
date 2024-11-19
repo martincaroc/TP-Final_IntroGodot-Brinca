d9916d5b9daa2fa7bce95577006932d6978492ab
Martín Caro <martincaro98@gmail.com>
2024-11-18 23:48:03 -0300

    [A] Add Snail enemy and Updated VisibleOnScreenEnabler to enemies

---
+++ Scenes/Enemies/snail.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 50.0
+var direction = -1
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	#Wall collision
+	if is_on_wall():
+		if get_wall_normal().x < 0:
+			direction = -1
+			$AnimatedSprite2D.flip_h = false
+		elif get_wall_normal().x > 0:
+			direction = 1
+			$AnimatedSprite2D.flip_h = true
+
+	velocity.x = direction * SPEED
+
+	move_and_slide()
